295b3d7bf6b2d2c3e77a7591dcea6f55c75bd849
Krishna Bogati <53785538+Coder-Krish@users.noreply.github.com>
2021-09-16 12:27:20 +0545

    generate bar code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as JsBarcode from 'jsbarcode';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BarCodeGeneratorApp';
+
+  public barcodeNumber:string = "";
+
+  public Width: number = 1.5;
+
+  public Height: number = 30;
+
+
+  public fontSize: number = 14;
+
+  public textMargin: number = 1;//this is not pixel, could be Percent.. (need to check.)
+
+  barcodeMargin: number = 10;
+
+  ngOnInit(){
+
+  }
+
+  generateBarcode(){
+    JsBarcode("#barcode", this.barcodeNumber, {
+      width: this.Width,
+      height: this.Height,
+      fontSize: this.fontSize,
+      textMargin: this.textMargin,
+      margin: this.barcodeMargin
+  });
+  }
+
 }
